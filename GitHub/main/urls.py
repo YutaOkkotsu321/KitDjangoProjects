7f9e8d46cce923d58749895699b578ae164ed0f5
YutaOkkotsu321 <dastan.k06g@gmail.com>
2026-04-12 20:56:52 +0500

    My project has finished. I added some functions

--- GitHub/main/urls.py
+++ GitHub/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path("", views.index),
-    path("addrepo/", views.repo)
+    path("", views.index, name='index'),
+    path("addrepo/", views.repo, name='repo'),
+    path("repo/<int:pk>/", views.repo_detail, name="repo_detail")
 ]
--- GitHub/main/views.py
+++ GitHub/main/views.py
@@ -1,5 +1,5 @@
 import os
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from newsapi import NewsApiClient
 from dotenv import load_dotenv
 from .models import Post
@@ -19,10 +19,25 @@ def index(request):
     return render(request, 'main/index.html', context=context)
 
 def repo(request):
+    repos = Post.objects.all()
+    context = {'repos': repos}
+   
     if request.method == 'POST':
-        title = request.POST.get('title', "").strip()
-        description = request.POST.get('description', "").strip()
-        if title and description:
-            Post.objects.create(title=title, description=description)
+        action = request.POST.get('action')
+        if action == 'clean-btn':
+            Post.objects.all().delete()
             return redirect(index)
-    return render(request, "main/repo.html")
+        elif action == 'add-btn':
+            title = request.POST.get('title', "").strip()
+            description = request.POST.get('description', "").strip()
+            if title and description:
+                Post.objects.create(title=title, description=description)
+                return redirect(index)
+    return render(request, "main/repo.html", context=context)
+def repo_detail(request, pk):
+    repos = Post.objects.all()
+    repo = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        Post.objects.all().delete()
+        return redirect(index)
+    return render(request, "main/repo_detail.html", context={'repo': repo, 'repos': repos})

eaf50c695e49155b6fb1688c3bd61285d6be9778
YutaOkkotsu321 <dastan.k06g@gmail.com>
2026-04-14 13:35:25 +0500

    fixed: window stopped closing

--- todoapp/main/static/main/index.js
+++ todoapp/main/static/main/index.js
@@ -1,5 +1,6 @@
 const btn_addTask = document.querySelector('.btn-add')
 const div_container_with_button = document.querySelector('#container-add-btn')
+const form = document.querySelector('#container-add-btn form')
 btn_addTask.addEventListener('click', () => {
     if(div_container_with_button.className == "work-add-task") {
         div_container_with_button.className = "hidden-add-task";
@@ -7,4 +8,16 @@ btn_addTask.addEventListener('click', () => {
         div_container_with_button.className = "work-add-task"
     }
     
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form)
+    fetch('', {
+        method: "POST",
+        body: formData,
+        headers: {'X-CSRFToken': formData.get('csfrmiddlewaretoken')}
+    }).then(() => {
+        form.reset();
+    })
 })

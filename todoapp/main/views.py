00fb91534580b4b4637b63fff92d8d1815a01293
YutaOkkotsu321 <dastan.k06g@gmail.com>
2026-04-14 13:16:50 +0500

    feat: add task submission to database

--- todoapp/main/views.py
+++ todoapp/main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from datetime import date
-
+from .models import ToDo
 
 # Create your views here.
 def main(request):
+    if request.method == "POST":
+        task = request.POST.get("task_input")
+        if task: 
+            ToDo.objects.create(title=task)
+
     cnt_time = date.today()
     return render(request, "main/index.html", { 'cnt_time': cnt_time })
 
